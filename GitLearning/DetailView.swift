322e87de25c42d777b6af3a7135d9e3e869b06ae
DNcode999 <danielngoho@icloud.com>
2025-07-30 22:19:42 +0800

    added close button on detailview

--- GitLearning/DetailView.swift
+++ GitLearning/DetailView.swift
@@ -8,10 +8,23 @@
 import SwiftUI
 
 struct DetailView: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
         Text("An App to Learn Git")
         
-    }
+        Button(action: {
+            dismiss()
+        }, label: {
+            ZStack {
+               RoundedRectangle(cornerRadius: 16)
+                    .frame(width: 350, height: 50)
+                Text("Close")
+                    .foregroundColor(.white)
+            }
+           
+        })
+     }
 }
 
 #Preview {
