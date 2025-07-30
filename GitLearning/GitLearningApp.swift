8eed60b24e56d32337f3dcf7c0287abac1220770
DNcode999 <danielngoho31@gmail.com>
2025-07-30 20:27:46 +0800

    Initial Commit

---
+++ GitLearning/GitLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLearningApp.swift
+//  GitLearning
+//
+//  Created by Daniel Joshua Ngoho on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct GitLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
