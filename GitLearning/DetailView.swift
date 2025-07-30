684998288aaf9f11eedd2e982b3f02966520d726
DNcode999 <danielngoho@icloud.com>
2025-07-30 20:58:45 +0800

    Added navigationLink to ContentView and added DetailVIew

---
+++ GitLearning/DetailView.swift
@@ -0,0 +1,19 @@
+//
+//  DetailView.swift
+//  GitLearning
+//
+//  Created by Daniel Joshua Ngoho on 7/30/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("An App to Learn Git")
+        
+    }
+}
+
+#Preview {
+    DetailView()
+}
